fba2ed266e537b7055ba36a944d88c6a5191b8c1
Dcterrance88 <>
2021-01-30 16:39:42 -0500

    FormsModule
    por medio del ejemplo del formulario se dio a entender la funcionalidad
    del ngSubmit del modulo FormModule el cual se comparo haciendo la misma
    funcionalidad de forma tradicional con JS.

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,5 +1,11 @@
+/* Importaciones Propias de Angular */
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+/* Importaciones de Terceros */
+
+/* Importaciones Propias */
 import { MainPageComponent } from './main-page/main-page.component';
 
 
@@ -12,7 +18,8 @@ import { MainPageComponent } from './main-page/main-page.component';
     MainPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class DbzModule { }
--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
-  templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  templateUrl: './main-page.component.html'
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent{
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public agregar() {
+    console.log('Heyy - Funcion Agregar con ngSubmit');
   }
 
 }
+/*
+
+public agregar(event:any) {
+    event.preventDefault(); // 1
+    console.log('Heyy - Funcion Agregar');
+  }
+
+  $event: es de interes recibir el evento que se esta disparando, en este caso todo
+            el evento del formulario, caso parecido al (click) que puede recibir el
+            evento que eso incluye las cordenadas donde se hizo el click en el boton,
+            donde se encuentra fisicamente el boton, etc.
+            en pocas palabras se interpreta como "Quiero el evento que está disparando
+            ese evento"
+
+<-- prevee el comportamiento por defecto que tiene el submit de un form (no hace refresh del navegador web).
+*/
